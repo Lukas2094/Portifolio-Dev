5b3770e4ebc166249f597e14715c2679064c3a55
Lukas2094 <lukas2_8@hotmail.com>
2025-07-08 12:36:46 -0300

    add SSR pages

--- app/page.tsx
+++ app/page.tsx
@@ -1,4 +1,7 @@
 import { HomePage } from "@/components/Home";
+import React from "react";
+
+export const dynamic = 'force-dynamic';
 
 export default function Home() {
   return <HomePage />;
--- app/projects/page.tsx
+++ app/projects/page.tsx
@@ -1,6 +1,8 @@
 import { ProjectsPage } from "@/components/Projects";
 import { getGitHubProjects } from "@/lib/github";
 
+export const dynamic = 'force-dynamic';
+
 export default async function Projects() {
     const repos = await getGitHubProjects();
     return <ProjectsPage repos={repos} />;
